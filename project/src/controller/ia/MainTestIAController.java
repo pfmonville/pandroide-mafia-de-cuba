eac4f6a99bbc209aa15388f8b88d53c2fa05861c
Beatriz Rojas <bearojas95@gmail.com>
2017-05-04 11:48:51 +0200

    mainTestIAController pour tester checkList

--- project/src/controller/ia/MainTestIAController.java
+++ project/src/controller/ia/MainTestIAController.java
@@ -2,11 +2,13 @@ package controller.ia;
 
 import java.util.ArrayList;
 
+import controller.App;
+import model.Answer;
 import model.Box;
 import model.Player;
+import model.Question;
 import model.Role;
-import controller.App;
-import data.World;
+import model.Talk;
 
 public class MainTestIAController {
 
@@ -14,19 +16,19 @@ public class MainTestIAController {
 		// Initialisation de la boite que recoit le joueur courant
 		ArrayList<String> stringList = new ArrayList<String>();
 		stringList.add(App.rules.getNameLoyalHenchman());
-		stringList.add(App.rules.getNameLoyalHenchman());
 //		stringList.add(App.rules.getNameLoyalHenchman());
 //		stringList.add(App.rules.getNameLoyalHenchman());
-		stringList.add(App.rules.getNameLoyalHenchman());
-		stringList.add(App.rules.getNameAgentCIA());
-		stringList.add(App.rules.getNameAgentFBI());
+//		stringList.add(App.rules.getNameLoyalHenchman());
+//		stringList.add(App.rules.getNameLoyalHenchman());
+//		stringList.add(App.rules.getNameAgentCIA());
+//		stringList.add(App.rules.getNameAgentFBI());
 //		stringList.add(App.rules.getNameDriver());
-		stringList.add(App.rules.getNameDriver());
-		int nombreDeDiamantsDansBoite = 6;
+//		stringList.add(App.rules.getNameDriver());
+		int nombreDeDiamantsDansBoite = 10;
 		Box testBox = new Box(nombreDeDiamantsDansBoite, stringList);
 		
 		// Initialisation du joueur courant
-		int positionDuJoueur = 5; // inclus dans [1 ; n-1], le parrain est le joueur 0
+		int positionDuJoueur = 4; // inclus dans [2 ; n], le parrain est le joueur 1
 		Player p = new Player(new Role(""), positionDuJoueur, false, false);
 		
 		IAController iac = new IAController(p);
@@ -55,31 +57,37 @@ public class MainTestIAController {
 		
 		// Affichage des configBefore
 		System.out.println("*** ConfigBefore ***");
-		for (World al : iac.getConfigBefore()) {
-	        String appender = "";
-	        for (Integer i : al.getRolesDistribution()) {
-	            System.out.print(appender + App.rules.convertNumberIntoRoleName(i));
-	            appender = " ";
-	        }
-	        System.out.println("\t role ecarte : "+ App.rules.convertNumberIntoRoleName(al.getTokenMovedAside()));
-	    }
+//		for (World al : iac.getConfigBefore()) {
+//	        String appender = "";
+//	        for (Integer i : al.getRolesDistribution()) {
+//	            System.out.print(appender + App.rules.convertNumberIntoRoleName(i));
+//	            appender = " ";
+//	        }
+//	        System.out.println("\t role ecarte : "+ App.rules.convertNumberIntoRoleName(al.getTokenMovedAside()));
+//	    }
 		System.out.println("nb configBefore: "+iac.getConfigBefore().size() );
 		System.out.println("temps d'execution [ms] : "+endConfigBefore);
 		System.out.println();
 		
 		// Affichage des configAfter
 		System.out.println("*** ConfigAfter ***");
-		for (World al : iac.getConfigAfter()) {
-	        String appender = "";
-	        for (Integer i : al.getRolesDistribution()) {
-	            System.out.print(appender + App.rules.convertNumberIntoRoleName(i));
-	            appender = " ";
-	        }
-	        System.out.println();
-	    }
+//		for (World al : iac.getConfigAfter()) {
+//	        String appender = "";
+//	        for (Integer i : al.getRolesDistribution()) {
+//	            System.out.print(appender + App.rules.convertNumberIntoRoleName(i));
+//	            appender = " ";
+//	        }
+//	        System.out.println();
+//	    }
 		System.out.println("nb configAfter: "+iac.getConfigAfter().size() );
 		System.out.println("temps d'execution [ms] : "+endConfigAfter);
 		System.out.println();
+		
+		Question q = new Question(2, " ", new ArrayList<Integer>(), 0);
+		q.setTargetPlayer(2);
+		Answer a = new Answer(2, " ",  new ArrayList<Integer>());
+		a.setNbDiamondsAnswer(6);
+		Talk t = new Talk(q, a);
 		
 	}
 
