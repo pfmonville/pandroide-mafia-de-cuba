33d7a72798d92454d451736884e62d1bf8549e6c
ClaireSalome <claire.c.salome@gmail.com>
2017-05-15 23:40:58 +0200

    AgentStrategy 2nd degré : suite
    -choose hidden token
    - choose role
    - choose nb diamands

--- project/src/controller/ai/strategy/AgentStrategy_2ndDegree.java
+++ project/src/controller/ai/strategy/AgentStrategy_2ndDegree.java
@@ -25,6 +25,9 @@ public class AgentStrategy_2ndDegree  implements ISuspectStrategy{
 		return null;
 	}
 
+	
+	
+	
 	/*
 	 * tell the truth or follow bluff
 	 */
@@ -63,16 +66,132 @@ public class AgentStrategy_2ndDegree  implements ISuspectStrategy{
 	@Override
 	public HashMap<String, Double> chooseHiddenTokenToShow(Player player,
 			Lie lie) {
-		//TODO
-		return null;
+		/*
+		 * First player only :
+		 * if I have already chosen a role -> according to this role moved aside a token
+		 */
+		HashMap<String, Double> hiddenTokenProbabilitiesResponse = new HashMap<String, Double>();
+
+		if(lie.getFalseRoleName() != null){
+			
+			String myFalseRole = lie.getFalseRoleName() ;
+			// if he claims to be a loyal or godfather's driver -> moves aside an agent
+			if(myFalseRole.equals(App.rules.getNameLoyalHenchman()) || myFalseRole.equals(App.rules.getNameCleaner())
+					|| myFalseRole.equals(App.rules.getNameDriver())){
+				hiddenTokenProbabilitiesResponse.put(player.getRole().getName(), 0.9 );
+				hiddenTokenProbabilitiesResponse.put(App.rules.getNameNoRemovedToken(), 0.1);
+			}
+			if(myFalseRole.equals(App.rules.getNameAgentFBI()) || myFalseRole.equals(App.rules.getNameAgentCIA()) || myFalseRole.equals(App.rules.getNameAgentLambda())){
+				//say the truth
+				hiddenTokenProbabilitiesResponse.put(player.getRole().getHiddenToken(), 1.0);
+			}
+		} 
+		/*
+		 * I have not chosen a role yet -> I can moved aside what I want
+		 */
+		else {
+					
+			hiddenTokenProbabilitiesResponse.put(player.getRole().getName(), 0.35);
+			hiddenTokenProbabilitiesResponse.put(App.rules.getNameLoyalHenchman(), 0.35);
+			hiddenTokenProbabilitiesResponse.put(App.rules.getNameDriver(), 0.2);
+			hiddenTokenProbabilitiesResponse.put(App.rules.getNameNoRemovedToken(), 0.1);
+		}
+		
+		return hiddenTokenProbabilitiesResponse;
 	}
 
+	
+	
+	
+	
 	@Override
 	public HashMap<String, Double> chooseRoleToShow(Player player, Lie lie) {
-		// TODO Auto-generated method stub
-		return null;
+		/*
+		 * choose between loyal, driver, agent, street urchin
+		 * have to check which role is moved aside, my false box
+		 */
+		HashMap<String, Double> roleProbabilitiesResponse = new HashMap<String, Double>();
+
+		double lhProba = 0.65;
+		double dProba = 0.1;
+		double aProba = 0.25;
+		
+		int lhNb = 0;
+		int dNb = 0;
+		int aNb = 0;
+		
+		boolean isCleanerHere = false;
+		
+		/*
+		 * If I already set a false list of tokens
+		 * I randomly choose a role in this list
+		 */
+		if(lie.isTokensInBoxSet()){
+			lhNb = lie.getFalseBox().getCount(App.rules.getNameLoyalHenchman())
+					+ lie.getFalseBox().getCount(App.rules.getNameCleaner());
+			dNb = lie.getFalseBox().getCount(App.rules.getNameDriver());
+			aNb = lie.getFalseBox().getCount(App.rules.getNameAgentCIA()) 
+					+ lie.getFalseBox().getCount(App.rules.getNameAgentFBI())
+					+ lie.getFalseBox().getCount(App.rules.getNameAgentLambda());
+			
+			if(lie.getFalseBox().getTokens().contains(App.rules.getNumberOfCleaners())){
+				isCleanerHere = true;
+			}
+		}
+		else{
+			/*
+			 * Less tokens in the box than taken before me OR I'm the last player
+			 * I choose to show a token already taken AND if I'm last player I can become a SU
+			 */
+			if(player.getBox().getTokens().size() < App.rules.getTokens().size() - player.getBox().getTokens().size() 
+					|| player.getPosition() == App.rules.getCurrentNumberOfPlayer()){
+				lhNb = App.rules.getNumberOfLoyalHenchmen() - player.getBox().getCount(App.rules.getNameLoyalHenchman())
+						+ App.rules.getNumberOfCleaners() - player.getBox().getCount(App.rules.getNameCleaner());
+				dNb = App.rules.getNumberOfDrivers() - player.getBox().getCount(App.rules.getNameDriver());
+				aNb = App.rules.getNumberOfAgents() - (player.getBox().getCount(App.rules.getNameAgentCIA()) 
+						+ player.getBox().getCount(App.rules.getNameAgentFBI())
+						+ player.getBox().getCount(App.rules.getNameAgentLambda()));
+				
+				if(App.rules.getNumberOfCleaners() != 0 && !player.getBox().getTokens().contains(App.rules.getNameCleaner())){
+					isCleanerHere = true;
+				}
+			}
+			/*
+			 * More tokens in the box than taken before me
+			 * I choose to show a token which is in the box
+			 */
+			else{
+				lhNb = player.getBox().getCount(App.rules.getNameLoyalHenchman())
+						+ player.getBox().getCount(App.rules.getNameCleaner());
+				dNb = player.getBox().getCount(App.rules.getNameDriver());
+				aNb = player.getBox().getCount(App.rules.getNameAgentCIA()) 
+						+ player.getBox().getCount(App.rules.getNameAgentFBI())
+						+ player.getBox().getCount(App.rules.getNameAgentLambda());
+				
+				if(player.getBox().getTokens().contains(App.rules.getNameCleaner())){
+					isCleanerHere = true;
+				}
+			}
+		}
+		
+		roleProbabilitiesResponse = calculTokenResponseProbatilities(player, lhNb, dNb, aNb, lhProba, dProba, aProba, isCleanerHere);
+		
+		// If I'm last player I can pretend to be a street urchin
+		if(player.getPosition() == App.rules.getCurrentNumberOfPlayer()){
+			double suProba = 0.3;
+			for(Entry<String, Double> entry : roleProbabilitiesResponse.entrySet()){
+				roleProbabilitiesResponse.put(entry.getKey(), entry.getValue() - suProba * entry.getValue());
+			}
+			roleProbabilitiesResponse.put(App.rules.getNameStreetUrchin(), suProba);	
+		}
+		
+		return roleProbabilitiesResponse;
 	}
 
+	
+	
+	
+	
 	@Override
 	public HashMap<Integer, RoleProbaCouple> showAssumedRolesForAllPlayers() {
 		// TODO Auto-generated method stub
@@ -80,4 +199,58 @@ public class AgentStrategy_2ndDegree  implements ISuspectStrategy{
 	}
 	
 
+	
+	
+	
+	
+	private HashMap<String, Double> calculTokenResponseProbatilities(Player player, int lhNb, int dNb, int aNb, double lhProba, double dProba, double aProba, boolean isCleanerHere){
+		HashMap<String, Double> tokenResponseProbabilities = new HashMap<String, Double>();
+		int totalNb = lhNb + dNb + aNb;
+		
+		if(lhNb == totalNb){
+			if(isCleanerHere){
+				tokenResponseProbabilities.put(App.rules.getNameLoyalHenchman(), (lhNb - 1) * 100.0 / lhNb);
+				tokenResponseProbabilities.put(App.rules.getNameCleaner(), 100.0 / lhNb);
+			}else{
+				tokenResponseProbabilities.put(App.rules.getNameLoyalHenchman(), 1.0);
+			}
+			return tokenResponseProbabilities;
+		}else if(dNb == totalNb){
+			tokenResponseProbabilities.put(App.rules.getNameDriver(), 1.0);
+			return tokenResponseProbabilities;
+		}else if(aNb == totalNb){
+			if(App.rules.getNumberOfAgents() == 1){
+				tokenResponseProbabilities.put(App.rules.getNameAgentFBI(), 1.0);
+			}else{
+				tokenResponseProbabilities.put(App.rules.getNameAgentFBI(), 0.5);
+				tokenResponseProbabilities.put(App.rules.getNameAgentCIA(), 0.5);
+			}	
+			return tokenResponseProbabilities;
+		}else if(lhNb == 0){
+			lhProba /= 2;
+			dProba += lhProba;
+			aProba += lhProba;
+		}else if(dNb == 0){
+			dProba /= 2;
+			lhProba += dProba;
+			aProba += dProba;
+		}else if(aNb == 0){
+			aProba /= 2;
+			lhProba += aProba;
+			dProba += aProba;
+		}
+		
+		double totalSum = lhNb * lhProba + dNb * dProba + aNb * aProba;
+		if(isCleanerHere){
+			tokenResponseProbabilities.put(App.rules.getNameLoyalHenchman(), (lhNb - 1) * lhProba * lhNb / ( lhNb * totalSum) );
+			tokenResponseProbabilities.put(App.rules.getNameCleaner(), lhProba * lhNb / (lhNb * totalSum));
+		}else{
+			tokenResponseProbabilities.put(App.rules.getNameLoyalHenchman(), lhProba * lhNb / totalSum);
+		}
+		tokenResponseProbabilities.put(App.rules.getNameDriver(), dProba * dNb / totalSum);
+		tokenResponseProbabilities.put(player.getRole().getName(), aProba * aNb / totalSum);
+		
+		return tokenResponseProbabilities;
+	}
+	
 }
